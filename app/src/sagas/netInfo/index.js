241ab4afce77ea682ea50ec1a6500087a3af387c
Alex Barry <alex.barry@gmail.com>
2019-04-25 07:58:15 -0400

    Split out netInfo so there is a main saga and initializer

--- app/src/sagas/netInfo/index.js
+++ app/src/sagas/netInfo/index.js
@@ -1,5 +1,15 @@
 import { call, put, take } from "redux-saga/effects";
+import NetInfo from "@react-native-community/netinfo";
 import * as netInfoSubs from "./netInfoChannel";
+
+export function *initialize() {
+  try {
+    const isConnected = yield call(NetInfo.isConnected.fetch);
+    yield put({ type: "NETINFO_CHANGE", isConnected });
+  } catch (err) {
+    console.log('netInfo.initialize', err);
+  }
+}
 
 export function* isConnected() {
   const channel = yield call(netInfoSubs.isConnected);
